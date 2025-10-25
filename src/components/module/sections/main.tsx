fd651c0ca6025bc42e9b7a17569a4b7a4c3aaccc
J Cui <i@cjxol.com>
2025-10-25 22:51:37 +0100

    Update about

--- src/components/module/sections/main.tsx
+++ src/components/module/sections/main.tsx
@@ -4,11 +4,11 @@ export const Main = () => (
       <h1 className="mt-3 text-4xl font-extrabold sm:text-6xl md:text-7xl 2xl:text-8xl">
         BioHackathon Edinburgh
       </h1>
-      <p className="mt-6 max-w-3xl px-2 text-center text-sm sm:text-base 2xl:max-w-4xl 2xl:text-2xl">
-        BioHackathon Edinburgh is a first-of-its-kind, interdisciplinary event at the University of Edinburgh that connects <span className="font-semibold">biologists, programmers, and industry partners</span> through a series of preparatory workshops, networking sessions, and challenge-based team projects. The initiative aims to create a sustainable ecosystem for <span className="font-semibold">collaboration, innovation, and entrepreneurship</span> at the interface of life sciences and artificial intelligence.
-      </p>
       <p className="mt-10 text-lg font-medium sm:text-2xl 2xl:text-3xl">
         Spring 2026, TBC — Nucleus Building, University of Edinburgh
+      </p>
+      <p className="mt-6 max-w-3xl px-2 text-center text-sm sm:text-base 2xl:max-w-4xl 2xl:text-2xl">
+        BioHackathon Edinburgh is a first-of-its-kind, interdisciplinary event at the University of Edinburgh that connects <span className="font-semibold">biologists, programmers, and industry partners</span> through a series of preparatory workshops, networking sessions, and challenge-based team projects. The initiative aims to create a sustainable ecosystem for <span className="font-semibold">collaboration, innovation, and entrepreneurship</span> at the interface of life sciences and artificial intelligence.
       </p>
     </div>
   </div>
