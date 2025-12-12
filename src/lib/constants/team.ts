f9c89de2f7ba6541e5f9b2fcf2f41d8fbb786933
J Cui <i@cjxol.com>
2025-12-12 10:57:06 +0000

    Update teams

--- src/lib/constants/team.ts
+++ src/lib/constants/team.ts
@@ -27,6 +27,18 @@ export const TEAM_MEMBERS: TeamMember[] = [
     image: "/team/apple.jpg",
   },
   {
+    name: "Horace",
+    role: "Events",
+    bio: "",
+    image: "/team/horace.jpg",
+  },
+  {
+    name: "Javi",
+    role: "Events",
+    bio: "",
+    image: "/team/javi.jpg",
+  },
+  {
     name: "Elena",
     role: "Design/Marketing",
     bio: "A 1st year PhD student in Neuroscience with a background in Molecular Genetics and interested in how we can use science to improve our understanding of the natural world and apply this to develop better targeted & efficient therapeutics as well as sustainable solutions in production and manufacturing",
@@ -50,5 +62,11 @@ export const TEAM_MEMBERS: TeamMember[] = [
     bio: "meow",
     image: "/team/jinxuan.png",
     link: "https://www.cjxol.com/",
+  },
+  {
+    name: "Davide Michieletto",
+    role: "Advisor",
+    bio: "",
+    image: "/team/davide.jpg",
   },
 ];
