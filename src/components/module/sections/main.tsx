51cd90d06b39b291e070f0ce868da4226eaa4d08
J Cui <i@cjxol.com>
2026-02-18 19:21:34 +0000

    Application closed

--- src/components/module/sections/main.tsx
+++ src/components/module/sections/main.tsx
@@ -26,10 +26,8 @@ export const Main = () => (
         Opens to <span className="font-semibold">undergraduate, postgraduate taught, and postgraduate research students, postdocs, and academic staff</span> from <span className="font-semibold">all UK universities</span> interested in biological and computational innovation. <span className="underline"><a className="font-semibold" href="#:~:text=For%20participants,design%20thinking.">No coding experience required</a></span>.
       </p>
 
-      <div className="mt-8 flex w-full max-w-2xl items-center justify-center gap-6 sm:mt-10">
-        <a href="https://forms.gle/XTx73urXEnToMaV38" target="_blank" className="text-2xl font-semibold uppercase bg-blue-700 px-7 py-5 text-white shadow-lg transition-transform duration-200 hover:scale-105 hover:bg-blue-800">
-          Apply to Participate
-        </a>
+      <div className="mt-6 text-sm text-white opacity-70">
+        The <span className="font-semibold">application has now closed</span>. If you have applied, you will hear back from us by 20th February. If your application has been successful, you will need to confirm your attendance by 27th February.
       </div>
       <div className="mt-6 text-sm text-white opacity-70">
         <a href='https://ed-ac-uk.zoom.us/rec/share/d1KokWKCHh4d7G8MoMey2-RDgS5xH76n69EcGiozLMk1PbsQa_1la1jaTjXwi8rx.6r61kEJnHQP3O9O3' target="_blank" className="mt-6 text-sm font-medium text-white underline">
